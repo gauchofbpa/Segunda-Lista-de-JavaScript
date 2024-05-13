c45f85470ce2df97859337f6f4217a8dfb20e24a
Gaúcho <joao_c_caetano@estudante.sesisenai.org.br>
2024-05-13 07:51:27 -0300

    Criação do HTML, CSS e JS

---
+++ script.js
@@ -0,0 +1,24 @@
+// Questão 01 //
+function () {
+
+}
+
+// Questão 02 //
+function () {
+
+}
+
+// Questão 03 //
+function () {
+
+}
+
+// Questão 04 //
+function () {
+
+}
+
+// Questão 05 //
+function () {
+
+}
